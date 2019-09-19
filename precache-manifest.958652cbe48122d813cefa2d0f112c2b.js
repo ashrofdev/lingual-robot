dce6072c430e986b54023d58a092b434caf0dea5
First Last <salmanashraf853@gmail.com>
2019-09-19 16:05:34 +0100

    Updates

---
+++ precache-manifest.958652cbe48122d813cefa2d0f112c2b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13cd6645e625b54f1591d4044a0f2376",
+    "url": "/repository-name/index.html"
+  },
+  {
+    "revision": "629478745dc83e520012",
+    "url": "/repository-name/static/css/2.1416e129.chunk.css"
+  },
+  {
+    "revision": "e2f7cc16d58a115e341c",
+    "url": "/repository-name/static/css/main.b45df2f6.chunk.css"
+  },
+  {
+    "revision": "629478745dc83e520012",
+    "url": "/repository-name/static/js/2.40a8b625.chunk.js"
+  },
+  {
+    "revision": "e2f7cc16d58a115e341c",
+    "url": "/repository-name/static/js/main.eba3910e.chunk.js"
+  },
+  {
+    "revision": "6e2fc4b7d77a9c53df47",
+    "url": "/repository-name/static/js/runtime~main.f7634681.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/repository-name/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/repository-name/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/repository-name/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/repository-name/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/repository-name/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
